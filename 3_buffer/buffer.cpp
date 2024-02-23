e53d1629ff290a964e63e98297c003d73c57d876
changqi1 <changqing.li@intel.com>
2024-02-23 13:55:23 +0000

    Update README.

--- 3_buffer/buffer.cpp
+++ 3_buffer/buffer.cpp
@@ -4,8 +4,8 @@
 
 /*
 Keys:
-  1) buffer: Use sycl::buffer to create buffer on devices.
-  2) accessor: the accessor of buffer.
+  1) buffer: Use sycl::buffer to create buffer on devices. Buffers encapsulate data in a SYCL application across both devices and host.
+  2) accessor: the accessor is the mechanism to access buffer data.
 
 Advantages: Expresses clear data dependencies.
 Disadvantages: Using buffers is not as convenient as directly using pointers and arrays.
@@ -25,10 +25,26 @@ int main() {
                 a[i] = i;
             });
         }).wait();
+
+        for (int i = 0; i < N; i++)
+            std::cout << v[i] << " ";
+        std::cout << std::endl; // 0 0 0 0 ...
+
+        // Creating host accessor is a blocking call and will only return after all
+        // enqueued SYCL kernels that modify the same buffer in any queue completes
+        // execution and the data is available to the host via this host accessor.
+        sycl::host_accessor b(buf, sycl::read_only);
+        for (int i = 0; i < N; i++)
+            std::cout << b[i] << " ";
+        std::cout << std::endl; // 0 1 2 3 ...
     }
 
+    // When execution advances beyond this function scope, buffer destructor is
+    // invoked which relinquishes the ownership of data and copies back the data to
+    // the host memory.
     for (int i = 0; i < N; i++)
-        std::cout << v[i] << "\n";
+        std::cout << v[i] << " ";
+    std::cout << std::endl; // 0 1 2 3 ...
 
     return 0;
 }
