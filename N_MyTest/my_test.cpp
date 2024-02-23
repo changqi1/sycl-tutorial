8c5b608ecd72779aa1e3bafd0d64019990f336aa
changqi1 <changqing.li@intel.com>
2024-02-23 14:30:39 +0000

    Example N: Add custom device selector.

--- N_MyTest/my_test.cpp
+++ N_MyTest/my_test.cpp
@@ -8,7 +8,35 @@
 
 constexpr int N = 16;
 
-sycl::device getMyDevice() {
+class DeviceSelector {
+public:
+    DeviceSelector(std::string PCI_BDF_Address) : PCI_BDF_Address_(PCI_BDF_Address){
+        vendorName_ = "Intel";
+    };
+
+    int operator()(const sycl::device& dev) const {
+        int rating = 0;
+
+        if (dev.has(sycl::aspect::ext_intel_pci_address)) {
+            if (dev.is_gpu() && (dev.get_info<sycl::info::device::name>().find(vendorName_) != std::string::npos) &&
+                    dev.get_info<sycl::ext::intel::info::device::pci_address>() == PCI_BDF_Address_)
+                rating = 4;
+                return rating;
+        }
+
+        if (dev.is_gpu() && (dev.get_info<sycl::info::device::name>().find(vendorName_) != std::string::npos))
+            rating = 3;
+        else if (dev.is_gpu()) rating = 2;
+        else if (dev.is_cpu()) rating = 1;
+        return rating;
+    };
+
+private:
+    std::string vendorName_;
+    std::string PCI_BDF_Address_;
+};
+
+sycl::device getMyDevice(std::string PCI_BDF_Address) {
     auto platforms = sycl::platform::get_platforms();
     for (auto &platform : platforms) {
         auto devices = platform.get_devices();
@@ -16,7 +44,7 @@ sycl::device getMyDevice() {
             if (device.has(sycl::aspect::ext_intel_pci_address)) {
                 auto BDF  = device.get_info<sycl::ext::intel::info::device::pci_address>();
                 std::cout << "Device BDF : " << BDF  << std::endl;
-                if (BDF  == "0000:5b:00.0") {
+                if (BDF  == PCI_BDF_Address) {
                     return device;
                 }
             }
@@ -25,16 +53,17 @@ sycl::device getMyDevice() {
 }
 
 int main() {
-    sycl::queue q(getMyDevice());
-    int *data = sycl::malloc_shared<int>(N, q);
+    std::string PCI_BDF_Address = "0000:5b:00.0";
 
-    q.parallel_for(N, [=](auto i) {
-        data[i] = i;
-    }).wait();
+    // Option 1
+    sycl::queue q1(getMyDevice(PCI_BDF_Address));
+    std::cout << "Device: " << q1.get_device().get_info<sycl::info::device::name>() << "\n";
+    std::cout << "Device: " << q1.get_device().get_info<sycl::ext::intel::info::device::pci_address>() << "\n";
 
-    for (int i = 0; i < N; i++)
-        std::cout << data[i] << "\n";
-
-    sycl::free(data, q);
+    // Option 2
+    DeviceSelector selector(PCI_BDF_Address);
+    sycl::queue q2(selector);
+    std::cout << "Device: " << q2.get_device().get_info<sycl::info::device::name>() << "\n";
+    std::cout << "Device: " << q2.get_device().get_info<sycl::ext::intel::info::device::pci_address>() << "\n";
     return 0;
 }
